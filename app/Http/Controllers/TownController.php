bc0bb385a91db0dc5f38d2537cb99ea1a0197b81
Samson Rollo <sdrollo@up.edu.ph>
2024-05-20 05:08:32 +0800

    Create TownController

---
+++ app/Http/Controllers/TownController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class TownController extends Controller
+{
+
+}
