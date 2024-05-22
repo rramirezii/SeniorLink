632f876fc880529dd7ebeed423a6f1344954d648
SamsonRollo <sdrollo@up.edu.ph>
2024-05-22 22:19:15 +0800

    Fix typos in vue routes

--- seniorlink-vue/src/main.js
+++ seniorlink-vue/src/main.js
@@ -1,11 +1,23 @@
 import { createApp } from 'vue';
 import * as views from "./view.js";
+import { createRouter, createWebHistory } from 'vue-router';
+
 import api from '../axios.config.js'; // Import your configured Axios instance
+
+// import the routes
+import routes from './router.js';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
 
 const app = createApp(views.SuperAdminDashboard); //change this to landing page, the login
 
 // Make the api instance globally available
 app.config.globalProperties.$api = api; 
+
+app.use(router);
 
 // Mount the app
 app.mount('#app');
