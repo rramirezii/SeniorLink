613fe30bb32459a8c1a8cdfeac918b16292bd76e
SamsonRollo <sdrollo@up.edu.ph>
2024-05-23 00:36:54 +0800

    Add general .env for general use

--- seniorlink-vue/vue.config.js
+++ seniorlink-vue/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
