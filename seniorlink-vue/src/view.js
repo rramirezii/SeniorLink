1dd5898070a751ff6b931437fece000166e07f67
Andrei Luz Asoy <83564453+andreilb@users.noreply.github.com>
2024-05-24 14:29:31 +0800

    Update view.js

--- seniorlink-vue/src/view.js
+++ seniorlink-vue/src/view.js
@@ -2,9 +2,31 @@
 export { default as ViewTown } from './components/ViewTown.vue';
 // export { default as ViewClientPerBarangay } from './components/ViewClientPerBarangay.vue'; //missing
 export { default as ViewBarangayPerTown } from './components/ViewBarangayPerTown.vue';
+
+//dashboard
+export { default as SuperAdminDashboard } from './views/SuperAdminDashboard.vue';
+export { default as BarangayDashboard } from './views/BarangayDashboard.vue';
+export { default as EstablishmentTransaction } from './views/EstablishmentTransaction.vue';
+export { default as EstablishmentDashboard } from './views/EstablishmentDashboard.vue';
+export { default as TownDashboard } from './views/TownDashboard.vue';
+
+//create
+export { default as CreateTown } from './components/CreateTown.vue';
+export { default as CreateSuperAdmin } from './components/CreateSuperAdmin.vue';
+export { default as CreateEstablishment } from './components/CreateEstablishment.vue'; //create establishment
+export { default as CreateBarangay } from './components/CreateBarangay.vue';
+export { default as CreateClient } from './components/CreateClient.vue';
+export { default as CreateTransaction } from './components/CreateTransaction.vue';
+export { default as CreateProduct }  from './components/CreateProduct.vue'
+
+//view
+export { default as ViewTown } from './components/ViewPerTown.vue';
+export { default as ViewPerBarangay } from './components/ViewPerBarangay.vue'; //changed name
+export { default as ViewPerTown } from './components/ViewPerTown.vue';
 export { default as ViewClientList } from './components/ViewClientList.vue';
 export { default as ViewBarangay } from './components/ViewBarangay.vue';
 export { default as ViewSuperAdmin } from './components/ViewSuperAdmin.vue';
+export { default as ViewSuperAdmin } from './components/ViewSuperAdmin-wdelete.vue';
 export { default as ViewEstablishment } from './components/ViewEstablishment.vue';
 export { default as ViewEstablishmentViewButton } from './components/ViewEstablishment-viewbutton.vue';
 export { default as ViewBarangayUpdateDelete } from './components/ViewBarangay-wdelete.vue';
@@ -22,6 +44,10 @@ export { default as ClientAuth } from './components/ClientAuth.vue';
 export { default as ClientQR } from './components/ClientQR.vue';
 export { default as ClientProfile } from './components/ClientProfile.vue';
 export { default as UpdateBarangaySelect } from './components/UpdateBarangaySelect.vue';
+export { default as ViewTransaction } from './components/ViewProductsTransaction.vue';
+export { default as ViewClientListUpdateDelete } from './components/ViewClientListUpdateDelete.vue';
+
+//update
 export { default as UpdateClientSelect } from './components/UpdateClientSelect.vue';
 export { default as UpdateTown } from './components/UpdateTown.vue';
 export { default as UpdateTransaction } from './components/UpdateTransaction.vue';
@@ -29,6 +55,7 @@ export { default as UpdateTownSelf } from './components/UpdateTownSelf.vue';
 export { default as UpdateSuperAdmin } from './components/UpdateSuperAdmin.vue';
 export { default as UpdateEstablishment } from './components/UpdateEstablishment.vue';
 export { default as UpdateBarangay } from './components/UpdateBarangay.vue';
+export { default as UpdateBarangaySelf } from './components/UpdateBarangaySelf.vue';
 export { default as UpdateClient } from './components/UpdateClient.vue';
 // export { default as UpdateProduct } from './components/UpdateProduct.vue';
 // export { default as UpdateProductTransaction } from './components/UpdateProductTransaction.vue';
@@ -37,8 +64,21 @@ export { default as CreateEstablishment } from './components/CreateEstablishment
 export { default as CreateBarangay } from './components/CreateBarangay.vue';
 export { default as CreateClient } from './components/CreateClient.vue';
 export { default as LoginPassword } from './components/LoginPassword.vue';
+export { default as UpdateClientSelf } from './components/UpdateClientSelf.vue';
+export { default as UpdateProduct } from './components/UpdateProduct.vue';
+export { default as UpdateEstablishmentSelf } from './components/UpdateEstablishmentSelf.vue';
 
 export { default as ViewClientListUpdateDelete } from './components/ViewClientListUpdateDelete.vue';
 export { default as ViewEstablishmentUpdateDelete } from './components/ViewEstablishmentUpdateDelete.vue';
+//client
+export { default as ClientTransactions } from './views/ClientMain.vue';
+export { default as ClientDashboard } from './views/ClientSelectMenu.vue';
+export { default as ClientLogin } from './components/ClientLogin.vue';
+export { default as ClientAuth } from './components/ClientAuth.vue';
+export { default as ClientQR } from './components/ClientQR.vue';
+export { default as ClientProfile } from './components/ClientProfile.vue';
+export { default as PrintTransactions } from './components/PrintTransactions.vue';
+
+export { default as LoginPassword } from './components/LoginPassword.vue';
 
 //lacking create establishment
