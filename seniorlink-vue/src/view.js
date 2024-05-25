bea1b778487f1b8318104c4ecadd94b2bf767c5b
SamsonRollo <sdrollo@up.edu.ph>
2024-05-25 18:30:15 +0800

    Fix remaining errpr

--- seniorlink-vue/src/view.js
+++ seniorlink-vue/src/view.js
@@ -4,13 +4,13 @@ export { default as ViewBarangayPerTown } from './components/ViewBarangayPerTown
 //dashboard
 export { default as SuperAdminDashboard } from './views/SuperAdminDashboard.vue';
 export { default as BarangayDashboard } from './views/BarangayDashboard.vue';
-export { default as EstablishmentTransaction } from './views/EstablishmentTransaction.vue';
+// export { default as EstablishmentTransaction } from './views/EstablishmentTransaction.vue'; // NOT RESOLVED
 export { default as EstablishmentDashboard } from './views/EstablishmentDashboard.vue';
 export { default as TownDashboard } from './views/TownDashboard.vue';
 
 //create
 export { default as CreateTown } from './components/CreateTown.vue';
-export { default as CreateSuperAdmin } from './components/CreateSuperAdmin.vue';
+// export { default as CreateSuperAdmin } from './components/CreateSuperAdmin.vue'; //NOT RESOLVED
 export { default as CreateEstablishment } from './components/CreateEstablishment.vue'; //create establishment
 export { default as CreateBarangay } from './components/CreateBarangay.vue';
 export { default as CreateClient } from './components/CreateClient.vue';
@@ -18,12 +18,12 @@ export { default as CreateTransaction } from './components/CreateTransaction.vue
 export { default as CreateProduct }  from './components/CreateProduct.vue'
 
 //view
-export { default as ViewTown } from './components/ViewPerTown.vue';
+// export { default as ViewTown } from './components/ViewPerTown.vue'; // NOT RESOLVED
 export { default as ViewPerBarangay } from './components/ViewPerBarangay.vue'; //changed name
-export { default as ViewPerTown } from './components/ViewPerTown.vue';
+// export { default as ViewPerTown } from './components/ViewPerTown.vue'; // NOT RESOLED
 export { default as ViewClientList } from './components/ViewClientList.vue';
 export { default as ViewBarangay } from './components/ViewBarangay.vue';
-export { default as ViewSuperAdmin } from './components/ViewSuperAdmin-wdelete.vue';
+// export { default as ViewSuperAdmin } from './components/ViewSuperAdmin-wdelete.vue'; #NOT RESOLVED
 export { default as ViewEstablishment } from './components/ViewEstablishment.vue';
 export { default as ViewEstablishmentViewButton } from './components/ViewEstablishment-viewbutton.vue';
 export { default as ViewBarangayUpdateDelete } from './components/ViewBarangay-wdelete.vue';
@@ -48,19 +48,19 @@ export { default as UpdateTownSelf } from './components/UpdateTownSelf.vue';
 export { default as UpdateSuperAdmin } from './components/UpdateSuperAdmin.vue';
 export { default as UpdateEstablishment } from './components/UpdateEstablishment.vue';
 export { default as UpdateBarangay } from './components/UpdateBarangay.vue';
-export { default as UpdateBarangaySelf } from './components/UpdateBarangaySelf.vue';
+// export { default as UpdateBarangaySelf } from './components/UpdateBarangaySelf.vue'; #NOT RESOLVED
 export { default as UpdateClient } from './components/UpdateClient.vue';
 // export { default as UpdateProduct } from './components/UpdateProduct.vue';
 // export { default as UpdateProductTransaction } from './components/UpdateProductTransaction.vue';
 export { default as LoginPassword } from './components/LoginPassword.vue';
-export { default as UpdateClientSelf } from './components/UpdateClientSelf.vue';
+// export { default as UpdateClientSelf } from './components/UpdateClientSelf.vue'; #NOT RESOLVED
 export { default as UpdateProduct } from './components/UpdateProduct.vue';
-export { default as UpdateEstablishmentSelf } from './components/UpdateEstablishmentSelf.vue';
+// export { default as UpdateEstablishmentSelf } from './components/UpdateEstablishmentSelf.vue'; #NOT RESOLVED
 
 export { default as ViewEstablishmentUpdateDelete } from './components/ViewEstablishmentUpdateDelete.vue';
 //client
 export { default as ClientTransactions } from './views/ClientMain.vue';
-export { default as ClientDashboardSelectMenu } from './views/ClientSelectMenu.vue';
-export { default as PrintTransactions } from './components/PrintTransactions.vue';
+// export { default as ClientDashboardSelectMenu } from './views/ClientSelectMenu.vue'; #NOT RESOLVED
+// export { default as PrintTransactions } from './components/PrintTransactions.vue'; # NOT RESOLVED
 
 //lacking create establishment
